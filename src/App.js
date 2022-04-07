a0abe27fca684036c54e511f7be291ea20334418
Seth Mulia <smulia@r2hstudent.com>
2022-04-07 11:31:55 -0400

    Nav and footer code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./components/NavFoot.css";
+import InstaNav from './components/InstaNav';
+import InstaFooter from './components/InstaFooter';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
+    
+      
+    <Router>
     <div className="App">
-
+      <InstaNav/>
+      <Routes>
+        {/* <Route path="/home" element={<HomePage/>}/>
+        <Route path="/explore" element={<Wrapper/>}/> */}
+      </Routes>
+      <InstaFooter/>
     </div>
+    </Router>
+    
   );
 }
 
---
+++ src/components/InstaFooter.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./NavFoot.css";
+
+function InstaFooter() {
+    return <div className="footer">
+        <ul className="links">
+            <li>About</li>
+            <li>Help</li>
+            <li>Press</li>
+            <li>API</li>
+            <li>Jobs</li>
+            <li>Privacy</li>
+            <li>Terms</li>
+            <li>Location</li>
+            <li>Top Accounts</li>
+            <li>Hashtags</li>
+            <li>Language</li>
+        </ul>
+        <div className="copyRight">@ 2028 INSTAGRAM FROM FACEBOOK</div>
+    </div>
+}
+
+export default InstaFooter;
