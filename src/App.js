17b8e698a3ca65fb9c64344b37db5510ff0db542
Brenden Hampton <brendenhampton@R2Hs-MBP.lan>
2022-04-08 10:20:35 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Activity from "./components/activity";
 function App() {
   return (
     <div className="App">
-
+      <header className="App-header"></header>
     </div>
   );
 }
