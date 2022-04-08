f603cd00bbd35767583572ede247576bdd24e74f
Patrick Pang <patrickpang@Patricks-MacBook-Air.local>
2022-04-08 11:56:34 -0400

    changed color scheme and added hover effect

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -1,10 +1,35 @@
 import React, { useState } from "react";
-import './profile.css';
+// import "./profile.css";
 
 const Profile = () => {
   const [suggestionMenu, setSuggestionMenu] = useState(false);
   const handleToggle = () => {
     setSuggestionMenu(!suggestionMenu);
+  };
+
+  const [followingIr, setFollowingIr] = useState(false);
+  const toggleFollowingIr = () => {
+    setFollowingIr(!followingIr);
+  };
+
+  const [followingAl, setFollowingAl] = useState(false);
+  const toggleFollowingAl = () => {
+    setFollowingAl(!followingAl);
+  };
+
+  const [followingRick, setFollowingRick] = useState(false);
+  const toggleFollowingRick = () => {
+    setFollowingRick(!followingRick);
+  };
+
+  const [followingShine, setFollowingShine] = useState(false);
+  const toggleFollowingShine = () => {
+    setFollowingShine(!followingShine);
+  };
+
+  const [followingJared, setFollowingJared] = useState(false);
+  const toggleFollowingJared = () => {
+    setFollowingJared(!followingJared);
   };
 
   return (
@@ -31,7 +56,18 @@ const Profile = () => {
               src="https://thumbs.dreamstime.com/b/approved-icon-profile-verification-accept-badge-quality-check-mark-sticker-tick-vector-illustration-128840911.jpg"
             />
             <button className="messageButton">Message</button>
-            <button className="followButton">Follow</button>
+            {followingIr ? (
+              <button
+                className="suggestionFollowing"
+                onClick={toggleFollowingIr}
+              >
+                Following
+              </button>
+            ) : (
+              <button className="suggestionFollow" onClick={toggleFollowingIr}>
+                Follow
+              </button>
+            )}
             <button className="suggestion" onClick={handleToggle}>
               {suggestionMenu ? (
                 <i class="fa fa-angle-up" aria-hidden="true"></i>
@@ -56,7 +92,9 @@ const Profile = () => {
           <div className="bio">
             <p className="bioIr">Instagram Redesign</p>
             <p className="bioHashtag">#WhereThingsComeToLife</p>
-            <div className="bioFollowed"><span className="bioFollowedBy">Followed by </span>remmysip</div>
+            <div className="bioFollowed">
+              <span className="bioFollowedBy">Followed by </span>remmysip
+            </div>
           </div>
         </div>
       </div>
@@ -81,7 +119,21 @@ const Profile = () => {
                   <div className="suggestionRealName">
                     <p>Al Smith</p>
                   </div>
-                  <button className="suggestionFollow">Follow</button>
+                  {followingAl ? (
+                    <button
+                      className="suggestionFollowing"
+                      onClick={toggleFollowingAl}
+                    >
+                      Following
+                    </button>
+                  ) : (
+                    <button
+                      className="suggestionFollow"
+                      onClick={toggleFollowingAl}
+                    >
+                      Follow
+                    </button>
+                  )}
                 </div>
               </div>
 
@@ -101,7 +153,21 @@ const Profile = () => {
                   <div className="suggestionRealName">
                     <p>Rick Name</p>
                   </div>
-                  <button className="suggestionFollow">Follow</button>
+                  {followingRick ? (
+                    <button
+                      className="suggestionFollowing"
+                      onClick={toggleFollowingRick}
+                    >
+                      Following
+                    </button>
+                  ) : (
+                    <button
+                      className="suggestionFollow"
+                      onClick={toggleFollowingRick}
+                    >
+                      Follow
+                    </button>
+                  )}
                 </div>
               </div>
 
@@ -121,7 +187,21 @@ const Profile = () => {
                   <div className="suggestionRealName">
                     <p>Shine Range</p>
                   </div>
-                  <button className="suggestionFollow">Follow</button>
+                  {followingShine ? (
+                    <button
+                      className="suggestionFollowing"
+                      onClick={toggleFollowingShine}
+                    >
+                      Following
+                    </button>
+                  ) : (
+                    <button
+                      className="suggestionFollow"
+                      onClick={toggleFollowingShine}
+                    >
+                      Follow
+                    </button>
+                  )}
                 </div>
               </div>
 
@@ -141,7 +221,21 @@ const Profile = () => {
                   <div className="suggestionRealName">
                     <p>Jared Real</p>
                   </div>
-                  <button className="suggestionFollow">Follow</button>
+                  {followingJared ? (
+                    <button
+                      className="suggestionFollowing"
+                      onClick={toggleFollowingJared}
+                    >
+                      Following
+                    </button>
+                  ) : (
+                    <button
+                      className="suggestionFollow"
+                      onClick={toggleFollowingJared}
+                    >
+                      Follow
+                    </button>
+                  )}
                 </div>
               </div>
             </div>
@@ -161,51 +255,51 @@ const Profile = () => {
 
       <div className="gallery">
         <img
-          className="item"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8YmVhdXRpZnVsJTIwaW1hZ2VzJTIwb2YlMjBwbGFjZXMlMjB0byUyMG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
         />
         <img
-          className="item"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1443926818681-717d074a57af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwaW1hZ2VzJTIwb2YlMjBwbGFjZXMlMjB0byUyMG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
         />
         <img
-          className="item"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1495594059084-33752639b9c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YmVhdXRpZnVsJTIwaW1hZ2VzJTIwb2YlMjBwbGFjZXMlMjB0byUyMG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
         />
         <img
-          className="item"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1552323543-4cffa4ffffe3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGJlYXV0aWZ1bCUyMGltYWdlcyUyMG9mJTIwcGxhY2VzJTIwdG8lMjBuYXR1cmV8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
         />
         <img
-          className="item"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1531162328795-808523268d2e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJlYXV0aWZ1bCUyMGltYWdlcyUyMG9mJTIwcGxhY2VzJTIwdG8lMjBuYXR1cmV8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
         />
         <img
-          className="item"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1444021465936-c6ca81d39b84?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YmVhdXRpZnVsJTIwaW1hZ2VzJTIwb2YlMjBwbGFjZXMlMjB0byUyMG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
         />
         <img
-          className="item"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1570030990547-f6b13f3062ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
         />
         <img
-          className="item"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1553856622-d1b352e9a211?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
         />
         <img
-          className="item"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1495908333425-29a1e0918c5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
         />
         <img
-          className="item"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1611420182989-3020dec418be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=800&q=60"
         />
         <img
-          className="item"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1583125311319-300af4db4abc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=800&q=60"
         />
         <img
-          className="item"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1566704351700-da9069468d94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
         />
       </div>
