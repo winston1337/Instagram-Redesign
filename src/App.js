fd5d78dcde9482a686ad58b5495d9fc21e5dc19c
Andrew  Woods <andrewwoods@Andrews-MacBook-Pro.local>
2022-04-09 13:43:35 -0400

    debugged issues with packages that install incorrectly. got pages and routes to render

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react"
 // import ChatScreen from "./ChatScreen";
 // import Chat from "./components/Chat.js";
 import Chats from "./components/Chats.js"
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Wrapper from './components/explore/ExploreWrapper';
 import HomePage from './pages/HomePage';
@@ -14,16 +14,14 @@ import HomePage from './pages/HomePage';
 function App (){
     return (
         <div className="App">
-        <Router>
-          <Routes>
-            <Route path="/home" element={<HomePage/>}/>
-            <Route path="/explore" element={<Wrapper/>}/>
-            <Route path="/chat" element={<Chats/>}>
-              
-                  
-              </Route>
-              </Routes>
-        </Router>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Navigate to="/home" />}/>
+              <Route path="/home" element={<HomePage/>}/>
+              <Route path="/explore" element={<Wrapper/>}/>
+              <Route path="/chat" element={<Chats/>}/>
+            </Routes>
+          </Router>
         </div>
     )
 }
