b20422409d4e78c10bfdf7ed1881725f32a6051a
Patrick Pang <patrickpang@Patricks-MacBook-Air.local>
2022-04-10 16:34:17 -0400

    Fixed info to fit screen

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -39,7 +39,7 @@ const Profile = () => {
   };
 
   return (
-    <div style={{ maxWidth: "550px", margin: "0px auto" }}>
+    <div style={{ maxWidth: "675px", margin: "0px auto" }}>
       <div
         style={{
           display: "flex",
@@ -50,7 +50,12 @@ const Profile = () => {
       >
         <div>
           <img
-            style={{ width: "168px", height: "168px", borderRadius: "80px" }}
+            style={{
+              width: "168px",
+              height: "168px",
+              borderRadius: "80px",
+              marginRight: "20px",
+            }}
             src="https://images.unsplash.com/photo-1633113247735-45a969eb9266?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aW5zdGFncmFtfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
           />
         </div>
@@ -58,7 +63,7 @@ const Profile = () => {
           <div class="buttonContainer">
             <h2>instagramredesign</h2>
             <img
-              className="check" alt="img"
+              className="check"
               src="https://thumbs.dreamstime.com/b/approved-icon-profile-verification-accept-badge-quality-check-mark-sticker-tick-vector-illustration-128840911.jpg"
             />
             <button className="messageButton">Message</button>
@@ -76,9 +81,17 @@ const Profile = () => {
             )}
             <button className="suggestion" onClick={handleToggle}>
               {suggestionMenu ? (
-                <i class="fa fa-angle-up" aria-hidden="true"></i>
+                <img
+                  className="upArrow"
+                  src="/profileImage/upArrow.png"
+                  alt="upArrow"
+                />
               ) : (
-                <i class="fa fa-angle-down" aria-hidden="true"></i>
+                <img
+                  className="downArrow"
+                  src="/profileImage/downArrow.png"
+                  alt="downArrow"
+                />
               )}
             </button>
           </div>
@@ -113,7 +126,7 @@ const Profile = () => {
               <div class="card">
                 <div className="card-image">
                   <img
-                    className="suggestionImage" alt="img"
+                    className="suggestionImage"
                     src="https://images.unsplash.com/photo-1559244673-9cee88d551f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=800&q=60"
                   />
                 </div>
@@ -124,7 +137,7 @@ const Profile = () => {
                     </p>
                   </div>
                   <div className="suggestionRealName">
-                    <p>Al Smith</p>
+                    <p className="nameOfSuggestion">Al Smith</p>
                   </div>
                   {followingAl ? (
                     <button
@@ -147,7 +160,7 @@ const Profile = () => {
               <div class="card">
                 <div class="card-image">
                   <img
-                    className="suggestionImage" alt="img"
+                    className="suggestionImage"
                     src="https://images.unsplash.com/photo-1568429838920-de3a3aa8cf1c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=800&q=60"
                   />
                 </div>
@@ -158,7 +171,7 @@ const Profile = () => {
                     </p>
                   </div>
                   <div className="suggestionRealName">
-                    <p>Rick Name</p>
+                    <p className="nameOfSuggestion">Rick Name</p>
                   </div>
                   {followingRick ? (
                     <button
@@ -181,7 +194,7 @@ const Profile = () => {
               <div class="card">
                 <div class="card-image">
                   <img
-                    className="suggestionImage" alt="img"
+                    className="suggestionImage"
                     src="https://images.unsplash.com/photo-1485541653056-e688bdf8319e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzF8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=800&q=60"
                   />
                 </div>
@@ -192,7 +205,7 @@ const Profile = () => {
                     </p>
                   </div>
                   <div className="suggestionRealName">
-                    <p>Shine Range</p>
+                    <p className="nameOfSuggestion">Shine Range</p>
                   </div>
                   {followingShine ? (
                     <button
@@ -215,7 +228,7 @@ const Profile = () => {
               <div class="card">
                 <div class="card-image">
                   <img
-                    className="suggestionImage" alt="img"
+                    className="suggestionImage"
                     src="https://images.unsplash.com/photo-1465188035480-cf3a60801ea5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=800&q=60"
                   />
                 </div>
@@ -226,7 +239,7 @@ const Profile = () => {
                     </p>
                   </div>
                   <div className="suggestionRealName">
-                    <p>Jared Real</p>
+                    <p className="nameOfSuggestion">Jared Real</p>
                   </div>
                   {followingJared ? (
                     <button
@@ -255,13 +268,13 @@ const Profile = () => {
           marginTop: "1rem",
           marginBottom: "1rem",
           width: "160%",
-          marginLeft: "-100px",
+          marginLeft: "-12rem",
         }}
       ></hr>
 
       <div className="gridPosts">
         <img
-          className="grid" alt="img"
+          className="grid"
           src="https://w7.pngwing.com/pngs/877/110/png-transparent-magic-square-safety-net-rectangle-mathematics-black-and-white-grid-miscellaneous-angle-furniture-thumbnail.png"
         />
         <span class="posts">POSTS</span>
@@ -269,51 +282,51 @@ const Profile = () => {
 
       <div className="gallery">
         <img
-          className="profileImage" alt="img"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8YmVhdXRpZnVsJTIwaW1hZ2VzJTIwb2YlMjBwbGFjZXMlMjB0byUyMG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
         />
         <img
-          className="profileImage" alt="img"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1443926818681-717d074a57af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwaW1hZ2VzJTIwb2YlMjBwbGFjZXMlMjB0byUyMG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
         />
         <img
-          className="profileImage" alt="img"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1495594059084-33752639b9c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YmVhdXRpZnVsJTIwaW1hZ2VzJTIwb2YlMjBwbGFjZXMlMjB0byUyMG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
         />
         <img
-          className="profileImage" alt="img"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1552323543-4cffa4ffffe3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGJlYXV0aWZ1bCUyMGltYWdlcyUyMG9mJTIwcGxhY2VzJTIwdG8lMjBuYXR1cmV8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
         />
         <img
-          className="profileImage" alt="img"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1531162328795-808523268d2e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJlYXV0aWZ1bCUyMGltYWdlcyUyMG9mJTIwcGxhY2VzJTIwdG8lMjBuYXR1cmV8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
         />
         <img
-          className="profileImage" alt="img"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1444021465936-c6ca81d39b84?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YmVhdXRpZnVsJTIwaW1hZ2VzJTIwb2YlMjBwbGFjZXMlMjB0byUyMG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
         />
         <img
-          className="profileImage" alt="img"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1570030990547-f6b13f3062ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
         />
         <img
-          className="profileImage" alt="img"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1553856622-d1b352e9a211?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
         />
         <img
-          className="profileImage" alt="img"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1495908333425-29a1e0918c5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
         />
         <img
-          className="profileImage" alt="img"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1611420182989-3020dec418be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=800&q=60"
         />
         <img
-          className="profileImage" alt="img"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1583125311319-300af4db4abc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=800&q=60"
         />
         <img
-          className="profileImage" alt="img"
+          className="profileImage"
           src="https://images.unsplash.com/photo-1566704351700-da9069468d94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
         />
       </div>
