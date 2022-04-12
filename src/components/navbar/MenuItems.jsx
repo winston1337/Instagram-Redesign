2e8ce05377213d76b5d1bb730290996a6229ed48
Winston McHetz <wmchetz95@gmail.com>
2022-04-12 07:45:21 -0400

    changed texts

--- src/components/navbar/MenuItems.jsx
+++ src/components/navbar/MenuItems.jsx
@@ -5,7 +5,7 @@ export const MenuItems = [
         cName: 'nav-links'
     },
     {
-        title: 'Activities',
+        title: 'Activity',
         url: '/activity',
         cName: 'nav-links'
     },
